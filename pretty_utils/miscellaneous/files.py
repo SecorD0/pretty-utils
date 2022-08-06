47567320f525904f18f8b548ec6e34e754e1c8ba
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-08-07 00:35:46 +0300

    'read_lines' and 'read_json' functions were added

--- pretty_utils/miscellaneous/files.py
+++ pretty_utils/miscellaneous/files.py
@@ -1,4 +1,6 @@
+import json
 import os
+
 
 def touch(path: str, file: bool = False) -> bool:
     """
@@ -19,3 +21,25 @@ def touch(path: str, file: bool = False) -> bool:
         os.mkdir(path)
         return True
     return False
+
+
+def read_lines(path: str) -> list:
+    """
+    Read a file and return a list of lines.
+
+    :param str path: path to the file
+    :return list: the list of lines
+    """
+    with open(path) as f:
+        lines = f.readlines()
+    return lines
+
+
+def read_json(path: str) -> list or dict:
+    """
+    Read a JSON file and return a Python list or dictionary.
+
+    :param str path: path to the JSON file
+    :return list or dict: the Python list or dictionary
+    """
+    return json.load(open(path))
