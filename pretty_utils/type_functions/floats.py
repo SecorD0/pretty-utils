40451aaf62b9a28351592b2b5c9fd4dec2cb3418
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-10-16 00:18:09 +0300

    Upgrade 'float' functions

--- pretty_utils/type_functions/floats.py
+++ pretty_utils/type_functions/floats.py
@@ -1,31 +1,48 @@
-def randfloat(from_: float, to_: float, decimal_places: int = None) -> float:
+from decimal import Decimal
+
+
+def randfloat(from_: int or float, to_: int or float, step: int or float = 0.1) -> float:
     """
     Return a random float from the range.
 
-    :param from_: the minimum value
-    :param to_: the maximum value
-    :param decimal_places: round to N decimal places (don't round)
-    :return: the random float
+    :param int or float from_: the minimum value
+    :param int or float to_: the maximum value
+    :param int or float step: the step size (0.1)
+    :return float: the random float
     """
-    from random import uniform
 
-    if decimal_places:
-        return round(uniform(from_, to_), decimal_places)
-    return uniform(from_, to_)
+    from random import randint
+
+    from_ = Decimal(str(from_))
+    to_ = Decimal(str(to_))
+    step = Decimal(str(step))
+    rand_int = Decimal(str(randint(0, int((to_ - from_) / step))))
+    return float(rand_int * step + from_)
 
 
-def float_range(from_: int or float, to_: int or float, step: int or float) -> list:
+def float_range(from_: int or float, to_: int or float, step: int or float = 0.1) -> list:
     """
     Return a float range.
 
-    :param from_: a range start value (included)
-    :param to_: the range stop value (not included)
-    :param step: a step size
-    :return: the range list
+    :param int or float from_: a range start value
+    :param int or float to_: the range stop value, not included
+    :param int or float step: a step size (0.1)
+    :return float: the range list
     """
-    import decimal
-    from numpy import arange
 
-    decimal_places = decimal.Decimal(str(step)).as_tuple().exponent * -1
+    from_ = Decimal(str(from_))
+    to_ = Decimal(str(to_))
+    step = Decimal(str(step))
+    range_list = []
 
-    return [round(x, decimal_places) for x in arange(from_, to_, step)]
+    if from_ < to_:
+        while from_ < to_:
+            range_list.append(float(from_))
+            from_ += step
+
+    else:
+        while from_ > to_:
+            range_list.append(float(from_))
+            from_ += step
+
+    return range_list
