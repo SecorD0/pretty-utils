7e1d54a05ff677836e90b46605bd7e1f95dbc892
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-10-16 00:58:41 +0300

    Upgrade and fix 'string' functions

--- pretty_utils/type_functions/strings.py
+++ pretty_utils/type_functions/strings.py
@@ -1,14 +1,11 @@
-from typing import Optional
-
-
-def text_between(text: str, begin: str = '', end: str = '') -> Optional[str]:
+def text_between(text: str, begin: str = '', end: str = '') -> str:
     """
     Extract a text between strings.
 
-    :param text: a source text
-    :param begin: a string from the end of which to start the extraction
-    :param end: a string at the beginning of which the extraction should end
-    :return: the extracted text or None
+    :param str text: a source text
+    :param str begin: a string from the end of which to start the extraction
+    :param str end: a string at the beginning of which the extraction should end
+    :return str: the extracted text or empty string if nothing is found
     """
     try:
         if begin:
@@ -25,24 +22,28 @@ def text_between(text: str, begin: str = '', end: str = '') -> Optional[str]:
             end = len(text)
     except:
         end = len(text)
-    return text[start:end]
+
+    excerpt = text[start:end]
+    if excerpt == text:
+        return ''
+    return excerpt
 
 
 def del_ws(text: str) -> str:
     """
     Delete whitespaces.
 
-    :param text: a source text
-    :return: the text without whitespaces
+    :param str text: a source text
+    :return str: the text without whitespaces
     """
     return text.replace(" ", "")
 
 
-def format_number(number: int or str) -> str:
+def format_number(number: int or float) -> str:
     """
-    Return formated number like 3 392 233.9420.
+    Return formatted number like 3 392 233.9420.
 
-    :param number: a number for formating
-    :return: the text without whitespaces
+    :param int or float number: a number for formatting
+    :return str: the formatted number
     """
     return '{0:,}'.format(number).replace(',', ' ')
