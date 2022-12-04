bdc0a4b036e82688ef694332bc4ffbdb9f53d357
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-12-05 00:34:29 +0300

    The 'timeout_input' function now works for both Linux and Windows

--- pretty_utils/miscellaneous/inputting.py
+++ pretty_utils/miscellaneous/inputting.py
@@ -1,21 +1,41 @@
-import signal
+import multiprocessing
+import os
+import sys
+import time
 
 
-def interrupted():
-    raise InterruptedError
-
-
-def timeout_input(prompt: str, timeout: int, default_value: str = '') -> str:
-    signal.signal(signal.SIGALRM, interrupted)
-    signal.alarm(timeout)
-    action = default_value
+def get_input(stdin_fd, queue, prompt):
+    sys.stdin = os.fdopen(stdin_fd)
     try:
-        action = input(prompt)
+        text = input(prompt)
+        queue.put(text)
 
-    except InterruptedError:
-        pass
+    except:
+        queue.put()
 
-    finally:
-        print()
-        signal.alarm(0)
-        return action
+
+def timeout_input(prompt: str, timeout: int or float = 60, default_value: str = '') -> str:
+    """
+    Ask a user to enter a string, and if he doesn't do so in a certain amount of time, return the default value.
+    Works only in if __name__ == '__main__' construction.
+
+    :param str prompt: a prompt that will be displayed before the input request
+    :param int or float timeout: a timeout after which the default value will be returned
+    :param str default_value: a default value that will be returned after the timeout expires
+    :return str: the inputted or default value
+    """
+    text = default_value
+    queue = multiprocessing.Queue()
+    input_process = multiprocessing.Process(target=get_input, args=(sys.stdin.fileno(), queue, prompt))
+    input_process.start()
+    start = time.time()
+    while time.time() - start < timeout:
+        if not queue.empty():
+            text = queue.get()
+            break
+
+        time.sleep(0.05)
+
+    input_process.terminate()
+    print()
+    return text
