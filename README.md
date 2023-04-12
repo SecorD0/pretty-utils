c7d834250fbf831a970526be9e325618164a65de
SecorD <25284138+SecorD0@users.noreply.github.com>
2023-04-13 02:04:41 +0300

    1.1.10 | Add the 'end' argument to the 'timeout_input' function

--- README.md
+++ README.md
@@ -653,7 +653,8 @@ print(aiohttp_params(params))
 ⠀Accepted arguments:
 - `prompt (str)` — a prompt that will be displayed before the input request
 - `timeout (Union[int, float])` — a timeout after which the default value will be returned (60)
-- `default_value (str)` — a default value that will be returned after the timeout expires
+- `default_value (str)` — a default value that will be returned after the timeout expires (empty string)
+- `end (str)` — string appended after the last value (two newlines)
 
 ⠀Returns `str` — the inputted or default value.
 
--- pretty_utils/miscellaneous/inputting.py
+++ pretty_utils/miscellaneous/inputting.py
@@ -15,14 +15,15 @@ def get_input(stdin_fd, queue, prompt):
         pass
 
 
-def timeout_input(prompt: str, timeout: Union[int, float] = 60, default_value: str = '') -> str:
+def timeout_input(prompt: str, timeout: Union[int, float] = 60, default_value: str = '', end: str = '\n\n') -> str:
     """
     Ask a user to enter a string, and if he doesn't do so in a certain amount of time, return the default value.
     Works only in if __name__ == '__main__' construction.
 
     :param str prompt: a prompt that will be displayed before the input request
-    :param Union[int, float] timeout: a timeout after which the default value will be returned
-    :param str default_value: a default value that will be returned after the timeout expires
+    :param Union[int, float] timeout: a timeout after which the default value will be returned (60)
+    :param str default_value: a default value that will be returned after the timeout expires (empty string)
+    :param str end: string appended after the last value (two newlines)
     :return str: the inputted or default value
     """
     text = default_value
@@ -38,5 +39,5 @@ def timeout_input(prompt: str, timeout: Union[int, float] = 60, default_value: s
         time.sleep(0.05)
 
     input_process.terminate()
-    print()
+    print(end, end='')
     return text
